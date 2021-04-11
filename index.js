a1d3b3ab1acef0c3ab9e36d251d7c66f43e0b2c5
Andres <andresjml@gmail.com>
2021-04-11 10:43:35 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Add your code here
+// Add your code her
+function submitData(name, email){
+    let data = {
+        name: name,
+        email: email
+        };
+      
+        let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+      };
+      
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then( function ( object ) {
+            document.body.innerHTML = object[ "id" ]
+          } )
+        .catch( function ( error ) {
+            document.body.innerHTML = error.message
+          } )
+}
+  
+/*function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }*/
